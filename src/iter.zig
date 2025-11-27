9ce031c47b0f956e284b8f3e64760a92d7dc2dad
Mario Wenzl <sjr1.wenzl@hotmail.com>
2025-11-27 21:20:10 +0100

    made code more generic

--- src/iter.zig
+++ src/iter.zig
@@ -1,9 +1,20 @@
 const std = @import("std");
 const ByteArrayList = std.ArrayList(u8);
 
+fn ctx_element_type(ctx_type: type) type {
+    const next_element_type = @typeInfo(@TypeOf(ctx_type.next)).@"fn".return_type.?;
+    return @typeInfo(next_element_type).optional.child;
+}
+
+fn ctx_mapped_type(f_type: type) type {
+    return @typeInfo(@typeInfo(f_type).pointer.child).@"fn".return_type.?;
+}
+
 pub const ByteArrayListCtx = struct {
     data: ByteArrayList,
     index: usize,
+
+    const element_type = u8;
 
     pub fn init(data: ByteArrayList) @This() {
         return .{ .data = data, .index = 0 };
@@ -17,31 +28,31 @@ pub const ByteArrayListCtx = struct {
     }
 };
 
-fn apply(element: u8) u8 {
-    return element;
-}
-
-fn Map(data_type: type, f_type: type) type {
+fn Map(ctx_type: type, f_type: type) type {
     return struct {
-        data: data_type,
+        ctx: ctx_type,
         apply: f_type,
 
-        fn next(self: *@This()) ?u8 {
-            const next_element = self.data.next() orelse return null;
+        const element_type = ctx_mapped_type(f_type);
+
+        fn next(self: *@This()) ?element_type {
+            const next_element = self.ctx.next() orelse return null;
             return self.apply(next_element);
         }
     };
 }
 
-fn Take(data_type: type) type {
+fn Take(ctx_type: type) type {
     return struct {
-        data: data_type,
+        ctx: ctx_type,
         n: usize,
 
-        fn next(self: *@This()) ?u8 {
+        const element_type = ctx_element_type(ctx_type);
+
+        fn next(self: *@This()) ?element_type {
             if (self.n == 0) return null;
             self.n -= 1;
-            return self.data.next();
+            return self.ctx.next();
         }
     };
 }
@@ -58,12 +69,12 @@ pub fn Iter(ctx_t: type) type {
             return self.ctx.next();
         }
 
-        pub fn map(self: @This(), f: *const fn (u8) u8) Iter(Map(@TypeOf(self.ctx), @TypeOf(f))) {
-            return .{ .ctx = .{ .data = self.ctx, .apply = f } };
+        pub fn map(self: @This(), f: *const fn (ctx_t.element_type) u8) Iter(Map(@TypeOf(self.ctx), @TypeOf(f))) {
+            return .{ .ctx = .{ .ctx = self.ctx, .apply = f } };
         }
 
         pub fn take(self: @This(), n: usize) Iter(Take(@TypeOf(self.ctx))) {
-            return .{ .ctx = .{ .data = self.ctx, .n = n } };
+            return .{ .ctx = .{ .ctx = self.ctx, .n = n } };
         }
     };
 }
